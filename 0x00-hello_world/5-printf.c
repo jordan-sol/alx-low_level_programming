304100ad0bfafe0f83d38e17c5b051035224ec20
jordan-sol <yordanoszewdu01@gmail.com>
2022-04-07 22:43:21 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+* myhello[D[D[D[D[D [D[M[C[C[C[C[hello, printf task
+*R[[* Return:return 1 after program execution
+*/
+int main()[Dv(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
