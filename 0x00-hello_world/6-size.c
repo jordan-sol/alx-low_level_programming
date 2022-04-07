8fcb1e00a9b8870b148b3334832a22d8a4d285e0
jordan-sol <yordanoszewdu01@gmail.com>
2022-04-07 23:40:06 +0300

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
 *
-*main - Entry point
+* main - Entry point
 *
-*Return: always 0 (success)[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D [B[B[D [A[A[B[B[A[A[C[C[C[C[C[C[C[C[C[C[Cmain _
-[A[A[B[C[C[C[C[C[C[C[- Entry point[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D*[B[C[C[C[CReturn: [C[C[CAlways 0 (s[C[C[C[success)
+* Return: Always 0 (success)
 */
 int main(void)
 {
-printf("size of a char: Id byte{s)\n", sizeof([D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[%[C[C[C[C[C[C[([C[C[C[C[C[C[C[C[C[C[C[C[C[Cchar));
+printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %d byte(s)\n",sizeof(long int));
-printf("size of a flot: %dbyte(s)\n",sizeof(flot));[A[A[A[A[A[A[B[B[B[B[B[B[A[C[C[C[C[C[C[C[C[C
-[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-printf("size of a long long int: %d byte(s)\n" , sizeof(long long int));[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D [C[C[C[C[C[C[C[C[C[C sizeof(float));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dloat:      [C[C[C[C%d byte(s)\n" , sizeof(float));[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a float: %d byte(s)\n" , sizeof(float));
+printf("size of a long long int: %d byte(s)\n" , sizeof(long long int));
 return (0);
 }
