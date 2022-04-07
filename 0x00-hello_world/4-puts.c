42872e9e62a99b71426554cdbafc728fecac4a9a
jordan-sol <yordanoszewdu01@gmail.com>
2022-04-07 22:15:33 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+# include <stdio.h>
 /**
 *main -entry point
-*
-*Return:0 after printing the function
+*[A [B
+*Return:0 after printing the function[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D [C[DRe[C[Cturn:[ 0 af[C[C[C[C[C[C[C[C[C[C[C[C[C[C[after printing the function
 */
-int main(void)
+int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
