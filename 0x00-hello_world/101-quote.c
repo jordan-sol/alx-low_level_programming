d50ba9741b29c0ced862cf5961514f235d7824b8
jordan-sol <yordanoszewdu01@gmail.com>
2022-04-08 00:48:43 +0300

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+Write(1, "and that piece of art is useful \" - Dora korpar, 2015 - 10 -19\n", 59);
+return (1);
+}
