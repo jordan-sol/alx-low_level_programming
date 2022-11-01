c586567764e5a9fc137a429fcaf9c1d88f69f3df
jordan-sol <101702526+jordan-sol@users.noreply.github.com>
2022-11-01 10:40:04 +0300

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,71 +1,73 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Searches for a value in a sorted array of integers.
- *
- * @array: pointer to the first element of the array to search in.
- * @size: the number of elements in array.
- * @value: value to search for.
- *
- * Conditions:
- *     @array will be sorted in ascending order.
- *     @value might appear more than once in array
- *
- * Return: the index where value is located. -1 otherwise.
- */
+  * print_array - print array with limits in l and r
+  * @array: set of numbers
+  * @l: left limit
+  * @r: right limit
+  * Return: nothing
+  */
+void print_array(int *array, size_t l, size_t r)
+{
+	printf("Searching in array: ");
+	for (l = l; l < r; l++)
+		printf("%d, ", array[l]);
+	printf("%d\n", array[l]);
+}
 
+/**
+  * bi_se - search the first ocurrency of a value in the array recursively
+  * @array: set of numbers
+  * @l: left limit
+  * @r: right limit
+  * @value: value to search
+  * Return: return the first index located otherwise -1
+  */
+size_t bi_se(int *array, size_t l, size_t r, int value)
+{
+	int mid = 0, index = -1;
 
+	if (l > r)
+		return (-1);
+
+	print_array(array, l, r);
+	mid = (l + r) / 2;
+	if (array[mid] < value)
+	{
+		l = mid + 1;
+		index = bi_se(array, l, r, value);
+	}
+	else if (array[mid] > value)
+	{
+		r = mid - 1;
+		index = bi_se(array, l, r, value);
+	}
+	else
+		if (mid - 1 >= 0 && array[mid - 1] == array[mid])
+			index = bi_se(array, l, mid, value);
+		else
+			index = mid;
+
+	return (index);
+}
+
+/**
+  * advanced_binary - search the first ocurrency of a value in the array
+  * @array: set of numbers
+  * @size: size of the array
+  * @value: value to search
+  * Return: return the first index located otherwise -1
+  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL)
+	int index = -1;
+
+	if (array == NULL || size == 0)
 		return (-1);
 
-	return (rec_binary(array, 0, size - 1, value));
-}
+	index = bi_se(array, 0, size - 1, value);
 
-/**
- * rec_binary - searches for a value in a sorted array of integers using
- *              binary search in a recursive way.
- * @array: pointer to the first element of the array to search in.
- * @lower: lower index of the sub-array.
- * @upper: upper index of the sub-array
- * @value: value to search for.
- *
- * Return: the index where value is located. -1 otherwise.
- */
-
-int rec_binary(int *array, int lower, int upper, int value)
-{
-	int half = (lower + upper) / 2, idx = 0;
-
-	if (lower == upper && array[lower] == value)
-		return (lower);
-	else if (lower == upper)
-		return (-1);
-
-	printf("Searching in array: ");
-	print_array(array, lower, upper);
-	if (array[half] < value)
-		idx = rec_binary(array, half + 1, upper, value);
-	else
-		idx = rec_binary(array, lower, half, value);
-	return (idx);
-}
-
-
-/**
- * print_array - prints a slice of the array from @l to @r.
- * @array: is a pointer to the array to print.
- * @l: left-most index to start the printing from in the array.
- * @r: right-most index to print in the array.
- *
- * Return: void.
- */
-
-void print_array(int *array, int l, int r)
-{
-	printf("%d", array[l]);
-	for (l = l + 1; l <= r; l++)
-		printf(", %d", array[l]);
-	printf("\n");
+	if (index >= 0)
+		return (index);
+	return (-1);
 }
